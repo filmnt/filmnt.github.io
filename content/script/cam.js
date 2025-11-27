f1361530eaf3c252ea5ace32c53cc481ee8aec00
filmnt <filmnt@proton.me>
2025-11-27 10:38:26 +0900

    .

--- content/script/cam.js
+++ content/script/cam.js
@@ -8,9 +8,12 @@ function cameraonoff(){
 }
 function cameraon(){
   if(navigator.mediaDevices&&navigator.mediaDevices.getUserMedia){
-    navigator.mediaDevices.getUserMedia({video:true}).then(stream=>{
+    navigator.mediaDevices.getUserMedia({video:{facingMode:"user",width:{ideal:1280},height:{ideal:720}}}).then(stream=>{
       videoElem.srcObject=stream;
       videoElem.style.display='block';
+      videoElem.style.width='100%';
+      videoElem.style.height='100%';
+      videoElem.style.objectFit='cover';
       videoElem.style.transform='scale(1.5)';
       videoElem.style.transformOrigin='center';
       return videoElem.play();
